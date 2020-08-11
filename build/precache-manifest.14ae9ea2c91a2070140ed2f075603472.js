0736c07e9246dfa20a44fd9773bb8e2fafe30bb2
makskul <azwart111@gmail.com>
2020-08-11 20:10:42 +0300

    build for demo

---
+++ build/precache-manifest.14ae9ea2c91a2070140ed2f075603472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f29f553bd416b3d01facba04c0e5e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4d483f7a674932f8538",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "249a93ee98153d128902",
+    "url": "/static/js/2.5fe90276.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5fe90276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d483f7a674932f8538",
+    "url": "/static/js/main.77fd0f60.chunk.js"
+  },
+  {
+    "revision": "bc8f36e19daabf608d6d",
+    "url": "/static/js/runtime-main.392b944e.js"
+  }
+]);
